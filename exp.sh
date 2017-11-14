8f85ea172e3f0ad8dae0b7a3ec6e8e92c436ee97
gdoggg <gdoggg2032@gmail.com>
2017-11-15 00:29:55 +0800

    exp.sh

--- exp.sh
+++ exp.sh
@@ -3,7 +3,7 @@ echo "tf-idf for small"
 cd preprocessing
 python3 preprocessing_tf_idf_cos.py --input ../datasets/small/DBdoc.json --output ../db/small_tf_idf_cos
 cd ..
-python3 run.py --index ../db/small_tf_idf_cos --model vsm --output ./tf_idf_cos_small.run
+python3 run.py --index ./db/small_tf_idf_cos --model vsm --output ./tf_idf_cos_small.run
 echo "done tf-idf for small"
 
 # lm for small 
@@ -11,7 +11,7 @@ echo "lm for small"
 cd preprocessing
 python3 preprocessing_lm.py --input ../datasets/small/DBdoc.json --output ../db/small_lm
 cd ..
-python3 run.py --index ../db/small_lm --model lm --output ./lm_small.run
+python3 run.py --index ./db/small_lm --model lm --output ./lm_small.run
 echo "done lm for small"
 
 # tf-idf for all
@@ -19,7 +19,7 @@ echo "tf-idf for all"
 cd preprocessing
 python3 preprocessing_tf_idf_cos.py --input ../datasets/all/DBdoc.json --output ../db/all_tf_idf_cos
 cd ..
-python3 run.py --index ../db/all_tf_idf_cos --model vsm --output ./tf_idf_cos_all.run
+python3 run.py --index ./db/all_tf_idf_cos --model vsm --output ./tf_idf_cos_all.run
 echo "done tf-idf for all"
 
 # lm for all
@@ -27,5 +27,5 @@ echo "lm for all"
 cd preprocessing
 python3 preprocessing_lm.py --input ../datasets/all/DBdoc.json --output ../db/all_lm
 cd ..
-python3 run.py --index ../db/all_lm --model lm --output ./lm_all.run
+python3 run.py --index ./db/all_lm --model lm --output ./lm_all.run
 echo "done lm for all"
